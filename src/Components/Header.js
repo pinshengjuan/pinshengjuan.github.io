342963736fb20531950b5334aafdd002884cf6b1
pinshengjuan <pinshengjuan@gmail.com>
2021-06-01 00:42:56 +0800

    change name header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -41,7 +41,7 @@ class Header extends Component {
                <Typing.Delay ms={200*1000} />
             </Typing>
             </span></h3>
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h3 className="responsive-headline">I'm {name}.</h3>
             {/* <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3> */}
             <hr />
             <ul className="social">
